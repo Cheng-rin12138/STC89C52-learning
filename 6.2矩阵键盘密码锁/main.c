e8d36653785e79182e1b49921539291be0b74c5d
Cheng-rin12138 <2562659316@qq.com>
2026-04-26 21:18:27 +0800

    矩阵键盘密码锁

--- 6.2矩阵键盘密码锁/main.c
+++ 6.2矩阵键盘密码锁/main.c
@@ -4,6 +4,7 @@
 #include "MatrixKey.h"
 
 unsigned char Keynum=0;
+unsigned int Password,count;
 void main()
 {
 	LCD_Init();
@@ -15,9 +16,38 @@ void main()
 		{
 			if(Keynum<=10)
 			{
-				Keynum=Keynum%10;
+				if(count<4)
+				{	
+					Password*=10;
+					Password+=Keynum%10;
+					count++;
+				}	
+				LCD_ShowNum(2,1,Password,4);
 			}
-			LCD_ShowNum(2,1,Keynum,2);
+			if(Keynum==11)//确认键
+			{
+				if(Password==1314)
+				{
+					LCD_ShowString(1,11,"True  ");
+					count=0;
+					Password=0;
+					LCD_ShowNum(2,1,Password,4);
+				}
+				else
+				{
+					LCD_ShowString(1,11,"Error ");
+					count=0;
+					Password=0;
+					LCD_ShowNum(2,1,Password,4);
+				}
+			}
+			if(Keynum==12)//取消输入
+			{
+				LCD_ShowString(1,11,"Cancel");
+				count=0;
+				Password=0;
+				LCD_ShowNum(2,1,Password,4);
+			}
 		}
 	}
 }
