4d360ae78cc30e04c5234bbd17836749e42cb182
Cheng-rin12138 <2562659316@qq.com>
2026-08-01 12:34:46 +0800

    优化按键，实现温度报警器

---
+++ 13.2DS18B20温度报警器/OneWire.c
@@ -0,0 +1,71 @@
+#include <REGX52.H>
+sbit OneWire_DQ=P3^7;
+
+unsigned char OneWire_Init(void)
+{
+	unsigned char i;
+	unsigned char AckBit;
+	EA=0;
+	//1.进入初始化，先确保高电平再拉低
+	OneWire_DQ=1;
+	OneWire_DQ=0;
+	//2.延时至少480us后再释放，留出多余一点时间，设为500ms
+	i=247;while(--i);//延时500us
+	//3.主机释放总线15~60us后，从机拉低60~240us（取70us检测从机拉低电平）
+	OneWire_DQ=1;//主机释放
+	i=32;while(--i);//延时70us检测
+	AckBit=OneWire_DQ;//读取电平
+	i=247;while(--i);//延时500ms，需要保证第二个时间片>480us
+	EA=1;
+	return AckBit;
+}
+
+void OneWire_SendBit(unsigned char Bit)
+{
+	unsigned char i;
+	EA=0;
+	//1.主机先拉低60~120us后释放表示发送0；拉低1~15us后释放表示发送1
+	OneWire_DQ=0;
+	i=4;while(--i);//延时10us（实际14）
+	OneWire_DQ=Bit;
+	i=24;while(--i);//延时50us（实际54）
+	OneWire_DQ=1;//主机释放
+	//如果Bit=0则14us后为低电平，Bit为1则14us后释放为高电平
+	EA=1;
+}
+
+unsigned char OneWire_ReceiveBit(void)
+{
+	unsigned char i;
+	unsigned char Bit;
+	EA=0;
+	//主机先拉低1~15us释放并在15us读取高低电平
+	OneWire_DQ=0;
+	i=2;while(--i);//延时5us
+	OneWire_DQ=1;
+	i=2;while(--i);//延时5us
+	Bit=OneWire_DQ;//采样
+	i=24;while(--i);//延时50us
+	EA=1;
+	return Bit;
+}
+//发送1个字节
+void OneWire_SendByte(unsigned char Byte)
+{
+	unsigned char i;
+	for(i=0;i<8;i++)
+	{
+		OneWire_SendBit(Byte&(0x01<<i));
+	}
+}
+//接收1个字节
+unsigned char OneWire_ReceiveByte(void)
+{
+	unsigned char i;
+	unsigned char Byte=0x00;
+	for(i=0;i<8;i++)
+	{
+		if(OneWire_ReceiveBit()){Byte|=(0x01<<i);}
+	}
+	return Byte;
+}
