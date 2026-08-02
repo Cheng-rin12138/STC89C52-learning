ee2e288a6e12f21894003a86f4c3bee9fcbb0b71
Cheng-rin12138 <2562659316@qq.com>
2026-08-02 11:24:41 +0800

    直流电机驱动

---
+++ 15.2直流电机驱动/main.c
@@ -0,0 +1,42 @@
+#include <REGX52.H>
+#include "Delay.h"
+#include "Key.h"
+#include "Nixie.h"
+#include "Timer0.h"
+sbit Motor=P1^0;
+unsigned char Counter,Compare;
+unsigned char KeyNum,Speed;
+void main()
+{
+	Timer0_Init();
+	while(1)
+	{
+		KeyNum=Key();
+		if(KeyNum==1)
+		{
+			Speed++;
+			Speed%=4;
+			if(Speed==0){Compare=0;}
+			if(Speed==1){Compare=25;}
+			if(Speed==2){Compare=50;}
+			if(Speed==3){Compare=100;}
+		}
+		Nixie(1,Speed);
+	}
+}
+void Timer0_Routinue() interrupt 1
+{
+	unsigned char T0Count;
+	TL0=0x9C;
+	TH0=0xFF;
+	Counter++;
+	Counter%=100;
+	if(Counter<Compare){Motor=1;}
+	else{Motor=0;}
+	T0Count++;
+	if(T0Count>20)
+	{
+		T0Count=0;
+		Key_Loop();
+	}
+}
