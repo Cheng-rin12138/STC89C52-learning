18862387160b38d1b2085576661bc7b502762329
Cheng-rin12138 <2562659316@qq.com>
2026-08-02 09:29:43 +0800

    LED呼吸灯

---
+++ 15.1LED呼吸灯/main.c
@@ -0,0 +1,33 @@
+#include <REGX52.H>
+sbit LED=P2^0;
+void Delay(unsigned int t)
+{
+	while(t--);
+}
+void main()
+{
+	unsigned char Time,i;
+	while(1)
+	{
+		for(Time=0;Time<100;Time++)//整体周期调节占空比
+		{
+			for(i=0;i<20;i++)//每个亮度的延时
+			{
+				LED=0;//亮
+				Delay(Time);
+				LED=1;//灭
+				Delay(100-Time);
+			}
+		}
+		for(Time=100;Time>0;Time--)//整体周期调节占空比
+		{
+			for(i=0;i<20;i++)//每个亮度的延时
+			{
+				LED=0;//亮
+				Delay(Time);
+				LED=1;//灭
+				Delay(100-Time);
+			}
+		}
+	}
+}
